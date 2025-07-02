b0b79727b433e1020eac5b91163895dd4c5cbee7
Samarpan <eth_sign77@proton.me>
2025-07-03 00:53:11 +0530

    wip

--- script/CreateSiloV2LoopPosition.s.sol
+++ script/CreateSiloV2LoopPosition.s.sol
@@ -249,11 +249,13 @@ contract CreateSiloV2LoopPosition is Script, MerkleTreeHelper {
         address silo0 = 0xB19C9622c629f1d246abB1D76be6F79CC2537e32; // silo0
         address silo1 = 0x5CD6614d11C3dD84fb941E5BE2F5A454C88e32b6; // silo1
 
-        (, bytes memory data0) =
-            SILO_LENS.call(abi.encodeWithSignature("getLtv(address,address)", silo0, getAddress(sourceChain, "syUSD")));
+        (, bytes memory data0) = SILO_LENS.staticcall(
+            abi.encodeWithSignature("getLtv(address,address)", silo0, getAddress(sourceChain, "syUSD"))
+        );
         uint256 userLTVSilo0 = abi.decode(data0, (uint256));
-        (, bytes memory data1) =
-            SILO_LENS.call(abi.encodeWithSignature("getLtv(address,address)", silo1, getAddress(sourceChain, "syUSD")));
+        (, bytes memory data1) = SILO_LENS.staticcall(
+            abi.encodeWithSignature("getLtv(address,address)", silo1, getAddress(sourceChain, "syUSD"))
+        );
         uint256 userLTVSilo1 = abi.decode(data1, (uint256));
 
         console.log("PT-sUSDf silo ltv:", userLTVSilo0);
@@ -402,6 +404,193 @@ contract CreateSiloV2LoopPosition is Script, MerkleTreeHelper {
         flashloanLeafs[10].argumentAddresses[1] = getAddress(sourceChain, "boringVault");
 
         flashloanManageProofs = _getProofsUsingTree(flashloanLeafs, manageTree);
+    }
+
+    function _addLeafs(ManageLeaf[] memory leafs) internal {
+        ERC20[] memory feeAssets = new ERC20[](3);
+        feeAssets[0] = getERC20(sourceChain, "USDC");
+        feeAssets[1] = getERC20(sourceChain, "USDT");
+        feeAssets[2] = getERC20(sourceChain, "USDS");
+        _addLeafsForFeeClaiming(leafs, getAddress(sourceChain, "accountantAddress"), feeAssets, false);
+
+        ERC20[] memory bridgeAssets = new ERC20[](2);
+        bridgeAssets[0] = getERC20(sourceChain, "USDC");
+        bridgeAssets[1] = getERC20(sourceChain, "USDT");
+        ERC20[] memory feeTokens = new ERC20[](2);
+        feeTokens[0] = getERC20(sourceChain, "WETH");
+        feeTokens[1] = getERC20(sourceChain, "GHO");
+
+        _addCcipBridgeLeafs(leafs, ccipBaseChainSelector, bridgeAssets, feeTokens);
+        _addCcipBridgeLeafs(leafs, ccipArbitrumChainSelector, bridgeAssets, feeTokens);
+        _addCcipBridgeLeafs(leafs, ccipBscChainSelector, bridgeAssets, feeTokens);
+
+        _addInfiniV1Leafs(leafs, getAddress(sourceChain, "USDC"));
+        _addCurveLeafs(
+            leafs, getAddress(sourceChain, "USDC_USDf_Curve_Pool"), 2, getAddress(sourceChain, "USDC_USDf_Curve_Gauge")
+        );
+
+        unchecked {
+            leafIndex++;
+        }
+        leafs[leafIndex] = ManageLeaf(
+            getAddress(sourceChain, "sUSDf"),
+            false,
+            "approve(address,uint256)",
+            new address[](1),
+            "",
+            getAddress(sourceChain, "rawDataDecoderAndSanitizer")
+        );
+        leafs[leafIndex].argumentAddresses[0] = getAddress(sourceChain, "pendleRouter");
+
+        _addBalancerFlashloanLeafs(leafs, getAddress(sourceChain, "USDC"));
+        _addBalancerFlashloanLeafs(leafs, getAddress(sourceChain, "USDT"));
+        _addBalancerFlashloanLeafs(leafs, getAddress(sourceChain, "DAI"));
+        _addBalancerFlashloanLeafs(leafs, getAddress(sourceChain, "USDS"));
+        _addBalancerFlashloanLeafs(leafs, getAddress(sourceChain, "WETH"));
+
+        _addMorphoBlueSupplyLeafs(leafs, getBytes32(sourceChain, "syrupUSDC_USDC_915"));
+        _addMorphoBlueSupplyLeafs(leafs, getBytes32(sourceChain, "PT-syrupUSDC-28AUG2025_USDC_915"));
+        _addMorphoBlueSupplyLeafs(leafs, getBytes32(sourceChain, "PT-iUSD-4SEP2025_USDC_915"));
+        _addMorphoBlueCollateralLeafs(leafs, getBytes32(sourceChain, "syrupUSDC_USDC_915"));
+        _addMorphoBlueCollateralLeafs(leafs, getBytes32(sourceChain, "PT-syrupUSDC-28AUG2025_USDC_915"));
+        _addMorphoBlueCollateralLeafs(leafs, getBytes32(sourceChain, "PT-iUSD-4SEP2025_USDC_915"));
+
+        _addPendleMarketLeafs(leafs, getAddress(sourceChain, "pendle_iUSD_09_04_2025"), false);
+        _addPendleMarketLeafs(leafs, getAddress(sourceChain, "pendle_syrupUSDC_08_28_2025"), false);
+        _addPendleMarketLeafs(leafs, getAddress(sourceChain, "LP_sUSDf_9_25_2025"), false);
+
+        // 1inch assets;
+        address[] memory oneInchAssets = new address[](10);
+        oneInchAssets[0] = getAddress(sourceChain, "USDC");
+        oneInchAssets[1] = getAddress(sourceChain, "SUSDE");
+        oneInchAssets[2] = getAddress(sourceChain, "USDS");
+        oneInchAssets[3] = getAddress(sourceChain, "USDT");
+        oneInchAssets[4] = getAddress(sourceChain, "USDE");
+        oneInchAssets[5] = getAddress(sourceChain, "lvlUSD");
+        oneInchAssets[6] = getAddress(sourceChain, "RLP");
+        oneInchAssets[7] = getAddress(sourceChain, "USR");
+        oneInchAssets[8] = getAddress(sourceChain, "wstUSR");
+        oneInchAssets[9] = getAddress(sourceChain, "cUSDO");
+        SwapKind[] memory kind = new SwapKind[](10);
+        kind[0] = SwapKind.BuyAndSell;
+        kind[1] = SwapKind.BuyAndSell;
+        kind[2] = SwapKind.BuyAndSell;
+        kind[3] = SwapKind.BuyAndSell;
+        kind[4] = SwapKind.BuyAndSell;
+        kind[5] = SwapKind.BuyAndSell;
+        kind[6] = SwapKind.BuyAndSell;
+        kind[7] = SwapKind.BuyAndSell;
+        kind[8] = SwapKind.BuyAndSell;
+        kind[9] = SwapKind.BuyAndSell;
+        _addLeafsFor1InchGeneralSwapping(leafs, oneInchAssets, kind);
+        _addOdosSwapLeafs(leafs, oneInchAssets, kind);
+
+        address[] memory incentivesControllers = new address[](2);
+        incentivesControllers[0] = address(0);
+        incentivesControllers[1] = address(0);
+        _addSiloV2Leafs(leafs, getAddress(sourceChain, "silo_PT-sUSDf_25Sep_USDC_config"), incentivesControllers);
+
+        ERC20[] memory supplyAssets = new ERC20[](1);
+        supplyAssets[0] = getERC20(sourceChain, "SUSDE");
+        ERC20[] memory borrowAssets = new ERC20[](2);
+        borrowAssets[0] = getERC20(sourceChain, "USDC");
+        borrowAssets[1] = getERC20(sourceChain, "USDT");
+        _addAaveV3Leafs(leafs, supplyAssets, borrowAssets);
+
+        _addERC4626Leafs(leafs, ERC4626(getAddress(sourceChain, "SUSDE")));
+        _addERC4626Leafs(leafs, ERC4626(getAddress(sourceChain, "sUSDf")));
+    }
+}
+
+contract DepositOnSilo is Script, MerkleTreeHelper {
+    uint256 public privateKey;
+
+    address public rawDataDecoderAndSanitizerEthereum = 0x67903b2D5e32DD3caebaf885163De1E94F8F61D2;
+    address public rawDataDecoderAndSanitizerBase01 = 0x53F0b212d28320DD0aB504AbD6871941EFf5AD45;
+    address public rawDataDecoderAndSanitizerArbitrum01 = 0x53F0b212d28320DD0aB504AbD6871941EFf5AD45;
+    RolesAuthority internal rolesAuthority = RolesAuthority(0xf7F3ace7f6cA2Cb1E7ccbE3Bf2Da13D001D36fdF);
+    BoringVault internal boringVault = BoringVault(payable(0x279CAD277447965AF3d24a78197aad1B02a2c589));
+    LayerZeroTeller internal teller = LayerZeroTeller(0xaefc11908fF97c335D16bdf9F2Bf720817423825);
+    ManagerWithMerkleVerification internal manager =
+        ManagerWithMerkleVerification(0x9B3e565ffC70c4b72516BC2dbec4b3c790940CE8);
+    AccountantWithRateProviders internal accountant =
+        AccountantWithRateProviders(0x03D9a9cE13D16C7cFCE564f41bd7E85E5cde8Da6);
+    BoringOnChainQueue internal queue = BoringOnChainQueue(0xF632c10b19f2a0451cD4A653fC9ca0c15eA1040b);
+    BoringSolver internal solver = BoringSolver(0x1d82e9bCc8F325caBBca6E6A3B287fE586536805);
+    address agent = 0xF171cAf19B2a55B015a68D80C337a16216775509;
+
+    uint8 public constant MANAGER_ROLE = 1;
+    uint8 public constant MINTER_ROLE = 2;
+    uint8 public constant BURNER_ROLE = 3;
+    uint8 public constant MANAGER_INTERNAL_ROLE = 4;
+    uint8 public constant PAUSER_ROLE = 5;
+    uint8 public constant SOLVER_ROLE = 12;
+    uint8 public constant OWNER_ROLE = 8;
+    uint8 public constant MULTISIG_ROLE = 9;
+    uint8 public constant STRATEGIST_MULTISIG_ROLE = 10;
+    uint8 public constant STRATEGIST_ROLE = 7;
+    uint8 public constant UPDATE_EXCHANGE_RATE_ROLE = 11;
+    uint8 public constant GENERIC_PAUSER_ROLE = 14;
+    uint8 public constant GENERIC_UNPAUSER_ROLE = 15;
+    uint8 public constant PAUSE_ALL_ROLE = 16;
+    uint8 public constant UNPAUSE_ALL_ROLE = 17;
+    uint8 public constant SENDER_PAUSER_ROLE = 18;
+    uint8 public constant SENDER_UNPAUSER_ROLE = 19;
+    uint8 public constant CAN_SOLVE_ROLE = 31;
+    uint8 public constant ONLY_QUEUE_ROLE = 32;
+    uint8 public constant SOLVER_ORIGIN_ROLE = 33;
+
+    function setUp() external {
+        privateKey = vm.envUint("BORING_DEVELOPER");
+
+        vm.createSelectFork("mainnet");
+        setSourceChainName("mainnet");
+
+        setAddress(true, mainnet, "boringVault", address(boringVault));
+        setAddress(true, mainnet, "managerAddress", address(manager));
+        setAddress(true, mainnet, "manager", address(manager));
+        setAddress(true, mainnet, "accountantAddress", address(accountant));
+        setAddress(true, mainnet, "rawDataDecoderAndSanitizer", rawDataDecoderAndSanitizerEthereum);
+    }
+
+    function run() public {
+        ManageLeaf[] memory leafs = new ManageLeaf[](1024);
+        _addLeafs(leafs);
+        bytes32[][] memory manageTree = _generateMerkleTree(leafs);
+
+        vm.createSelectFork("mainnet");
+        setSourceChainName("mainnet");
+
+        uint256 ptBalance = getERC20(sourceChain, "PT_sUSDf_9_25_2025").balanceOf(address(boringVault));
+
+        vm.startBroadcast(vm.envUint("BORING_MORPHO_AGENT"));
+        {
+            address[] memory targets = new address[](1);
+            bytes[] memory targetData = new bytes[](1);
+            ManageLeaf[] memory manageLeafs = new ManageLeaf[](1);
+            uint256[] memory values = new uint256[](1);
+            address[] memory decodersAndSanitizers = new address[](1);
+
+            // deposit PT-sUSDf on silo0 of PT-sUSDf-25Sep-2025/USDC config
+            manageLeafs[0] = ManageLeaf(
+                0xB19C9622c629f1d246abB1D76be6F79CC2537e32,
+                false,
+                "deposit(uint256,address)",
+                new address[](1),
+                "",
+                getAddress(sourceChain, "rawDataDecoderAndSanitizer")
+            );
+            manageLeafs[0].argumentAddresses[0] = getAddress(sourceChain, "boringVault");
+            targets[0] = 0xB19C9622c629f1d246abB1D76be6F79CC2537e32; // silo0
+            targetData[0] = abi.encodeWithSignature("deposit(uint256,address)", ptBalance, address(boringVault));
+            decodersAndSanitizers[0] = getAddress(sourceChain, "rawDataDecoderAndSanitizer");
+
+            bytes32[][] memory manageProofs = _getProofsUsingTree(manageLeafs, manageTree);
+            manager.manageVaultWithMerkleVerification(manageProofs, decodersAndSanitizers, targets, targetData, values);
+        }
+        console.log("balance now:", getERC20(sourceChain, "PT_sUSDf_9_25_2025").balanceOf(address(boringVault)));
+
+        vm.stopBroadcast();
     }
 
     function _addLeafs(ManageLeaf[] memory leafs) internal {
